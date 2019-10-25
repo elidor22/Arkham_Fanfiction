959d3263a5c0ce7249b5861a8b80d27caec4347a
elidor22 <elidorv@outlook.com>
2019-10-25 21:19:08 +0200

    Update is completely functional

--- app/Http/Controllers/articles_controller.php
+++ app/Http/Controllers/articles_controller.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class articles_controller extends Controller
 {
+
     public function index()
     {
         return Article::all();
@@ -16,6 +17,7 @@ class articles_controller extends Controller
     public function show()
     {
         return Article::where('user_id', auth()->user()->id)->get();
+        Session::save();
     }
 
     public function store(Request $request)
@@ -29,23 +31,39 @@ class articles_controller extends Controller
       
       $article->save();
 
-      ddd($article);
+      /* dd(
+        auth()->id() ?? '?',
+        Auth::id() ?? '?',
+        $request->user()->id ?? '?',
+        auth()->check(),
+        get_class(auth()->guard())
+    ); */
       return $article;
     }
 
-    public function update(Request $request, $user_id)
+    public function update(Request $request)
     {
-        $article = Article::findOrFail($user_id);
-        $article->update($request->all());
+        $article = $request->validate([
+            'title' => 'required|string',
+            'article'=>'required|string',
+        ]);
+        $title=$request->title;
+
+        //Find if the conditions are met then updates the information
+        Article::where('user_id', auth()->user()->id)->where('title', $title)
+        ->update($article);
+       // $article->update($article);
         return $article;
         
     }
 
     public function delete(Request $request)
     {
-        $user_id = Auth::user();
-        $article = Article::where('user_id',$user_id)->first();
-        $article->delete();
+        $user_id =Auth::user()->id;
+        $title = $request->title;
+        $article = Article::where('user_id', $user_id)
+        ->where('title', $title);
+        $article->delete($article);
 
         return 204;
     }
--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::get('/article', 'articles_controller@index');
     Route::get('/article/specific','articles_controller@show');
     Route::post('article', 'articles_controller@store');
-    Route::patch('article/{id}', 'articles_controller@update');
+    Route::patch('article', 'articles_controller@update');
     Route::delete('article/delete', 'articles_controller@delete');
 
     //Route::match([ 'patch'], '/article/{id}', 'articles_controller@update');
